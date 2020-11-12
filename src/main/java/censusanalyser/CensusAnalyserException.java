7597e536f459cd10872103ce575f2e79171457e6
Nisha271 <bukkenisha@gmail.com>
2020-11-12 19:34:38 +0530

    [Archana] Added: UC2 with loading the India Code Csv with exceptions

--- src/main/java/censusanalyser/CensusAnalyserException.java
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -3,7 +3,7 @@ package censusanalyser;
 public class CensusAnalyserException extends Exception {
 
     enum ExceptionType {
-        CENSUS_FILE_PROBLEM,WRONG_FILE_TYPE,NO_SUCH_FILE
+		CENSUS_FILE_PROBLEM, WRONG_FILE_TYPE,NO_SUCH_FILE,UNABLE_TO_PARSE
     }
 
     ExceptionType type;
