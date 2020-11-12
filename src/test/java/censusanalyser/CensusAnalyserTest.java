bd106d255cedfb5106b0981edd92e3d984b22860
Nisha271 <bukkenisha@gmail.com>
2020-11-12 17:59:16 +0530

    [Archana] Added: UC1 with test cases to find no of records from csv file and for get wrong file type

---
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -0,0 +1,35 @@
+package censusanalyser;
+
+import static org.junit.Assert.assertEquals;
+
+import  org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class CensusAnalyserTest {
+
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\AB\\eclipse-workspace\\CensusAnalyser\\static\\IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            assertEquals(29,numOfRecords );
+        } catch (CensusAnalyserException e) { }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            @SuppressWarnings("deprecation")
+			ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+           assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+}
